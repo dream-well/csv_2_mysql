a11b701bac38af3e8bbd088b07435f615a7d24ac
dream-well <dreamwell506@outlook.com>
2023-03-21 04:59:14 +0900

    add express

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import csv2mysql from './csv2mysql';
+import express from 'express';
+import { readdirSync } from 'fs';
+import path from 'path';
 
-csv2mysql("test");
+const app = express();
+
+app.get("/convert", async (req, resp) => {
+    const files = readdirSync(process.env.csv_folder).filter(name => name.endsWith(".csv"));
+    console.log(files);
+    for(let file of files) {
+        resp.write(`converting ${file}\n`);
+        await csv2mysql(path.join(process.env.csv_folder, file.substr(0, file.length-4)));
+    }
+    resp.end(`Ended`);
+});
+
+app.listen(5000, () => {
+    console.log("app is listening");
+})
