a4f42bf46e0af6d3b9d0dec45e528505ff2e7a61
dream-well <dreamwell506@outlook.com>
2023-03-22 03:36:54 +0900

    table name

--- index.ts
+++ index.ts
@@ -26,6 +26,6 @@ void async function main() {
     const files = readdirSync(process.env.csv_folder).filter(name => name.endsWith(".csv"));
     console.log(files);
     for(let file of files) {
-        await csv2mysql(path.join(process.env.csv_folder, file.substr(0, file.length-4)));
+        await csv2mysql(path.join(process.env.csv_folder, file.substr(0, file.length-4)), file);
     }
 }()
